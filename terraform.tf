61c440d404ad42d149a3a4c9e5b2755079f6f39c
Amulya hn <Amulya hn>
2021-08-24 16:51:11 +0530

    added backend

--- terraform.tf
+++ terraform.tf
@@ -5,8 +5,16 @@ terraform {
    required_providers {
 		aws = {
 		  version = "=3.55.0"
-		 source = "hashicorp/aws"
+		  source = "hashicorp/aws"
 
 		}
 	}
+	
+	backend "s3" {
+		bucket = "s3-test-backend-bucket-ammu"
+		key = "backend/terraform.tfstate"
+		region = "us-east-2"
+		dynamodb_table = "backend_dynamoDB"
+		encrypt = true
+    }
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,23 @@ variable "ami-id" {
 	type = string 
 	default = "ami-00399ec92321828f5"
 }
+
+variable "bucketname" {
+	type = string
+	default = "s3-test-backend-bucket-ammu"
+}
+
+variable "dynamoname" {
+	type = string
+	default = "backend_dynamoDB"
+}
+
+variable "aws_region" {
+	type = string
+	default = "us-east-1"
+}
+
+variable "hash_key" {
+	type = string
+	default = "LockID"
+}
